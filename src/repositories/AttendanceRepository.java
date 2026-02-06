e78c9b2efd3118b5b3648058fae3826455a68c24
tauyekelulyshalkar <tauekelulyshalkar@gmail.com>
2026-02-06 05:52:17 +0500

    Shalkar, create AttendanceRepository interface

---
+++ src/repositories/AttendanceRepository.java
@@ -0,0 +1,12 @@
+package repositories;
+
+import entities.Attendance;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface AttendanceRepository {
+    void save(Attendance attendance);
+    boolean exists(int memberId, LocalDate date);
+    List<Attendance> findByMemberId(int memberId);
+}
